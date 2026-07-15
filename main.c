1b05c3574d7e8f61710fd5e8842dabe0ac9d6ab2
Nguyen Quoc Thanh Vinh <thanhvinhabi132@gmail.com>
2026-07-15 11:27:00 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    // SHOPPING CART PROGRAM
+    
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
+
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
+
+    printf("How many would you like? ");
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+
+
+    printf("\nYou have bought %d %s", quantity, item);
+    printf("%c%.2f", currency, total);
+
+}
